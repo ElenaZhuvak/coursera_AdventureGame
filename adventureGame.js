7c3e9505e18f4441bdc5c7da1151a8980c624b22
Elena Zhuvak <elenkazh@gmail.com>
2026-01-06 21:40:53 +0000

    add comments

--- adventureGame.js
+++ adventureGame.js
@@ -1,15 +1,3 @@
-const readline = require("readline-sync");
-
-//Get player name using readline-sync
-let playerName = "";
-let playerHealth = 100;
-let playerGold = 20;
-let currentLocation = 'village';
-let gameRunning = true;
-let inventory = [];
-
-//Create variables for player stats
-
 /*
 Adventure Game
 This game will be a text-based game where the player will be able
@@ -18,14 +6,27 @@ The player will be able to choose their own path and the story will change
 based on their decisions.
 */
 
+const readline = require("readline-sync");
+
+//Create variables for player stats
+let playerName = "Max";
+let playerHealth = 100;
+let playerGold = 20;
+let currentLocation = 'village';
+let gameRunning = true;
+let inventory = [];
+
+
+
 // Display the game title
 console.log("Welcome to the Adventure Game");
 
 // Add a welcome message
 console.log("Prepare yourself for an epic journey!");
 
+//Get player name using readline-sync
 playerName = readline.question("What is your name, brave adventure?");
-console.log("Welcome " + playerName + "to the game!");
-console.log("You started with " + playerGold + "gold!");
 
 // Display welcome message and starting stats
+console.log("Welcome " + playerName + " to the game!");
+console.log("You started with " + playerGold + " gold!");
